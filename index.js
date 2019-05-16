603ea1288abcdddd8d78e8e91fc7a4ff70a1e2a9
William Chang <wilicw1346@gmail.com>
2019-05-16 14:54:03 +0800

    add phone menu

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 let _ = document
+let $ = i=>_.getElementById(i)
 let app = {
   "title": "大安高工電腦研究社 | DACSC",
   "name": "大安高工電腦研究社",
@@ -12,19 +13,35 @@ let app = {
     {"text": "關於電研", "link": "/indexDev.html#about"}
   ]
 }
+let menushow = false
+let menuhtml = ""
 let items = ["Algorithm", "Assembly", "C++", "Python", "Javascript", "<b>DACSC</b>"]//, "Your Grandma"]
 items.forEach(i=>{
-  _.getElementById('typed-strings').innerHTML += `<p>We Love <br class="phone"><span class="color">${i}</span>.</p>`
+  $('typed-strings').innerHTML += `<p>We Love <br class="phone"><span class="color">${i}</span>.</p>`
 })
 app.menu.forEach(i=>{
-  _.getElementById('menu').innerHTML += `<p class="title nonphone menu"><a href="${i.link}">${i.text}</a></p>`
+  $('menu').innerHTML += `<p class="title nonphone menu"><a href="${i.link}">${i.text}</a></p>`
 })
+
 _.title = app.title
 _.getElementsByClassName('nonphone')[0].innerHTML = app.name
 _.getElementsByClassName('phone')[0].innerHTML = app.shortname
-_.getElementById('footer').innerHTML = `© ${app.year} <b>${app.footer}</b>.`
+$('footer').innerHTML = `© ${app.year} <b>${app.footer}</b>.`
 let typed = new Typed('#typed', {
   stringsElement: '#typed-strings',
   typeSpeed: 30,
   backSpeed: 15
 })
+
+app.menu.forEach(i=>{
+  menuhtml += `<p><a href="${i.link}">${i.text}</a></p>`
+})
+
+let showmenu = ()=>{
+  if (!menushow) {
+    $('phonemenu').innerHTML = menuhtml
+  } else {
+    $('phonemenu').innerHTML = ""
+  }
+  menushow = !menushow
+}
