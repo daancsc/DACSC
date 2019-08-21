f7ec001488778c13850ec99d955530bdc7aa4a3b
William Chang <wilicw1346@gmail.com>
2019-08-21 14:39:06 +0800

    add news

--- pages/default.js
+++ pages/default.js
@@ -6,14 +6,36 @@ export function page (app) {
     <div class="cover container mono">
       <div id="typed-strings"></div>
       <span id="typed"></span>
+      <br>
+      <p class="news">
+
+      </p>
       <div id="contact"></div>
     </div>
+    <style>
+      .news {
+        font-size: 1.5em;
+      }
+    </style>
   `
   items.forEach(i => {
     $('#typed-strings').innerHTML += `<p>We Love <br class="phone"><span class="color">${i}</span>!</p>`
   })
   contact.forEach(i => {
     $('#contact').innerHTML += `<a target="_blank" rel="noopener noreferrer" href="${i.link}"><i class="${i.icon}" aria-hidden="true"></i></a>`
+  })
+  news.forEach(i => {
+    let today = new Date()
+    let yyyy = today.getFullYear()
+    let mm = today.getMonth()+1
+    let dd = today.getDate()
+    if (!(yyyy>=i.deadline.yyyy&&mm>=i.deadline.mm&&dd>=i.deadline.dd)) {
+      $('.news').innerHTML += `
+        <a target="_blank" rel="noopener noreferrer" href="${i.link}">
+          <span class="color"><b>News</b></span> ${i.title}
+        </a><br>
+      `
+    }
   })
   app.modules.startTyping()
 }
@@ -32,5 +54,17 @@ let contact = [
     name: 'github',
     link: 'https://github.com/daancsc/DACSC',
     icon: 'fab fa-github'
+  }
+]
+
+let news = [
+  {
+    title: '<span class="color">8/24 </span>新生試探課程報名',
+    link: 'https://docs.google.com/forms/d/e/1FAIpQLSeBS8woh0f-hcwoweHdeaUNA2Qv1M8xN6q5Wyx4QgpqSRK9kQ/viewform',
+    deadline: {
+      yyyy: 2019,
+      mm: 8,
+      dd: 24
+    }
   }
 ]
