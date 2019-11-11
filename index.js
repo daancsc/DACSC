5a63003f8bf7f7e0381ac4cb91a6de816ce9573c
William Chang <william20021026@gmail.com>
2019-11-11 22:44:50 +0800

    Change icon when in dark mode

--- index.js
+++ index.js
@@ -147,4 +147,10 @@ _.addEventListener('DOMContentLoaded', () => {
   pangu.autoSpacingPage()
 })
 
+if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+  $('#favicon').href = "logo.png"
+} else {
+  $('#favicon').href = "logo_black.png"
+}
+
 console.log('%c DACSC\n 大安電研\n 21st\n ', 'font-family: "Ubuntu Mono", monospace;font-size: 5em; color: red;text-shadow: 3px 3px 0 rgb(217,31,38) , 6px 6px 0 rgb(226,91,14) , 9px 9px 0 rgb(245,221,8) , 12px 12px 0 rgb(5,148,68) , 15px 15px 0 rgb(2,135,206) , 18px 18px 0 rgb(4,77,145) , 21px 21px 0 rgb(42,21,113)')
