cbf4b35f093636dcda88d37a57078456562109de
Toby1009 <69885352+Toby1009@users.noreply.github.com>
2020-08-29 15:15:34 +0800

    Update friends.js

--- pages/friends.js
+++ pages/friends.js
@@ -72,7 +72,7 @@ let friends = {
     },
     { name: '中山女高資訊研究社',
       shortname: 'ZSISC',
-      fb: 'zsisc25',
+      fb: '中山女高資訊研究社26th-267152867499225',
       ig: 'zsisc26th',
       web: ''
     },
